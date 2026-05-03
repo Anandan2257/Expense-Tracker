f18f7d8f5636505a1b912040be80c8be0f3a33f2
anandanperiasamy <anandan.periasamy@zohocorp.com>
2026-05-03 16:11:07 +0530

    add new changes

--- server/index.js
+++ server/index.js
@@ -22,3 +22,5 @@ app.get('/api/health', (req, res) => res.json({ status: 'ok' }));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
